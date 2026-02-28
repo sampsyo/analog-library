7d5be5190c76a0718648b88b8cea19d4eee43fb7
Adrian Sampson <adrian@radbox.org>
2026-02-28 11:50:11 -0500

    Start doing some minijinja stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use axum::{Router, extract::Path, routing::get};
+use axum::{
+    Router,
+    extract::{Path, State},
+    routing::get,
+};
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct DOIData {
     title: String,
 }
 
-async fn show_doi(Path(doi): Path<String>) -> String {
+async fn show_doi(State(ctx): State<Context>, Path(doi): Path<String>) -> String {
     // TODO validate DOI
     let doi_url = format!("https://doi.org/{doi}");
 
@@ -25,11 +29,21 @@ async fn show_doi(Path(doi): Path<String>) -> String {
     format!("hi {}", res.title)
 }
 
+#[derive(Clone)]
+struct Context {
+    tmpls: minijinja::Environment<'static>,
+}
+
 #[tokio::main]
 async fn main() {
+    let ctx = Context {
+        tmpls: minijinja::Environment::new(),
+    };
+
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/doi/{*doi}", get(show_doi));
+        .route("/doi/{*doi}", get(show_doi))
+        .with_state(ctx);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8118").await.unwrap();
     axum::serve(listener, app).await.unwrap();
