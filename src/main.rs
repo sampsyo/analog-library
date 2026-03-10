adc99b6f12c1676f57dbf9de2bc9fa85aef10d61
Adrian Sampson <adrian@radbox.org>
2026-03-09 21:42:18 -0400

    Subcommands

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,16 @@ mod webcache;
 use core::Context;
 
 fn main() {
+    let mut args = pico_args::Arguments::from_env();
     let ctx = Context::default();
-    serve::serve(ctx);
+    match args.subcommand().unwrap().as_deref() {
+        None | Some("serve") => {
+            serve::serve(ctx);
+        }
+        Some(cmd) => {
+            eprintln!("unknown command {cmd}");
+            eprintln!("available commands are: serve");
+            std::process::exit(1);
+        }
+    }
 }
