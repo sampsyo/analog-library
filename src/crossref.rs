140503b41c467bc4be929216d4a7428adcaf1a52
Adrian Sampson <adrian@radbox.org>
2026-03-14 22:30:20 -0400

    Don't panic

--- src/crossref.rs
+++ src/crossref.rs
@@ -149,25 +149,26 @@ impl Display for Date {
             [y, m, d] => write!(f, "{} {d}, {y}", month(m)),
             [y, m] => write!(f, "{} {y}", month(m)),
             [y] => write!(f, "{y}"),
-            _ => panic!("date should have 1, 2, or 3 parts"),
+            _ => Ok(()),
         }
     }
 }
 
+const MONTHS: [&str; 12] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
 fn month(n: u32) -> &'static str {
-    match n {
-        1 => "January",
-        2 => "February",
-        3 => "March",
-        4 => "April",
-        5 => "May",
-        6 => "June",
-        7 => "July",
-        8 => "August",
-        9 => "September",
-        10 => "October",
-        11 => "November",
-        12 => "December",
-        _ => panic!("month out of range"),
-    }
+    MONTHS.get(n as usize).copied().unwrap_or("?")
 }
