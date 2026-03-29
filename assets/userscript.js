e50b223deccc14b320a854c2f43359a29ba7e577
JJ <git@toki.la>
2026-03-29 00:41:04 -0700

    Fix userscript redirect

--- assets/userscript.js
+++ assets/userscript.js
@@ -3,9 +3,9 @@
 // @description     Redirect the ACM Digital Library to an Analog Library interface
 // @author          apropos
 // @version         1.0
-// @include         *://dl.acm.org/*
+// @include         *://dl.acm.org/doi/*
 // @run-at          document-start
 // ==/UserScript==
 
 // This is a userscript. It requires a userscript manager, such as ViolentMonkey.
-window.location.replace("__HOST__" + window.location.pathname);
+window.location.replace("http://__HOST__" + window.location.pathname);
