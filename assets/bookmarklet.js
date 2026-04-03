78c1789b092607eebda5a8ae7e73d84c9142fde5
Adrian Sampson <adrian@radbox.org>
2026-04-03 16:01:07 -0400

    Better normalize DL URLs in redirects
    Make the redirects work on specific full-text DL pages too.

--- assets/bookmarklet.js
+++ assets/bookmarklet.js
@@ -1,3 +1,9 @@
 (function () {
-  window.location.replace("http://__HOST__" + window.location.pathname);
+  let path = window.location.pathname;
+  if (path.startsWith("/doi/")) {
+    path = path.replace(/\/(abs|pdf|epdf|fullHtml)\//, "/");
+    window.location.replace("http://__HOST__" + path);
+  } else {
+    console.error("this path does not look like a DOI");
+  }
 })();
