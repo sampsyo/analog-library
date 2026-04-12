5d48d1a01ca69ccf07d309ca4ad074635b31af01
Adrian Sampson <adrian@radbox.org>
2026-04-12 15:35:01 -0400

    Use `-v` for verbose logging
    ...instead of `RUST_LOG`.

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,35 @@ enum MainError {
     Jats(#[from] jats::Error),
 }
 
-async fn run() -> Result<(), MainError> {
-    tracing_subscriber::fmt::fmt()
+/// Set up logging to the console.
+fn log_setup(verbose: bool) {
+    use tracing::Level;
+    use tracing::level_filters::LevelFilter;
+    use tracing_subscriber::filter::Targets;
+    use tracing_subscriber::fmt::Subscriber;
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::util::SubscriberInitExt;
+
+    // This is awkward because of two weird behaviors in tracing_subscriber: (1) it
+    // defaults to logging to stdout instead of stderr, and (2) when you try to fix
+    // that, the builder doesn't use `RUST_LOG` by default. Instead of mucking
+    // around with `RUST_LOG` stuff, we just use a flag to enable debug logging.
+
+    let level = if verbose { Level::DEBUG } else { Level::INFO };
+    let targets = Targets::new().with_target(module_path!(), level);
+
+    let subscriber = Subscriber::builder()
+        .with_max_level(LevelFilter::TRACE)
         .with_writer(std::io::stderr)
-        .init();
+        .finish()
+        .with(targets);
+
+    subscriber.init();
+}
+
+async fn run() -> Result<(), MainError> {
     let mut args = pico_args::Arguments::from_env();
+    log_setup(args.contains("-v"));
     let ctx = Context::default();
     match args.subcommand()?.as_deref() {
         None | Some("serve") => {
